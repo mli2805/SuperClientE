0b329d7b40efd98b0899bc220196017f49b4d1b4
mli2805 <mli2805@gmail.com>
2018-07-16 16:32:05 +0300

    ChildStarter refactored

--- SuperClientCa/ChildStarter.cs
+++ SuperClientCa/ChildStarter.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+using Panel = System.Windows.Forms.Panel;
 
 namespace SuperClientCa
 {
     public class ChildStarter
     {
+        private const int GwlStyle = -16;
+        private const int WsBorder = 0x00800000;
+        private const int WsChild = 0x40000000;
+        private const int SwShowmaximized = 3;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(HandleRef hWnd, HandleRef hWndParent);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+
         const string ClientFilename = @"c:\VsGitProjects\SuperClientE\LittleClient\bin\Debug\LittleClient";
         //        const string ClientFilename = @"c:\VsGitProjects\Fibertest\Client\WpfClient\bin\Debug\Iit.Fibertest.Client.exe";
 
       
+        public Panel CreatePanel(TabItem tabItem)
+        {
+            var windowsFormsHost = new WindowsFormsHost();
+            tabItem.Content = windowsFormsHost;
 
+            Panel panel = new Panel();
+            windowsFormsHost.Child = panel;
+
+            return panel;
+        }
+
+        public void PutChildOnPanel(Process childProcess, Panel panel)
+        {
+            IntPtr childHandle = childProcess.MainWindowHandle;
+            int oldStyle = GetWindowLong(childHandle, GwlStyle);
+            SetWindowLong(childHandle, GwlStyle, (oldStyle | WsChild) & ~WsBorder);
+            var parentHandle = new HandleRef(null, panel.Handle);
+            SetParent(new HandleRef(null, childHandle), parentHandle);
+            var childHandleRef = new HandleRef(null, childHandle);
+            ShowWindowAsync(childHandleRef, SwShowmaximized);
+        }
 
         public Process StartChild(int number, string serverIp, string serverPort)
         {
@@ -23,7 +65,7 @@ namespace SuperClientCa
             process.Start();
             //            var pause = number == 3 ? 45 : 10;
             var pause = 2;
-            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(pause));
+            Thread.Sleep(TimeSpan.FromSeconds(pause));
             return process;
        //     return process.MainWindowHandle;
         }
--- SuperClientCa/ShellViewModel.cs
+++ SuperClientCa/ShellViewModel.cs
@@ -12,23 +12,6 @@ namespace SuperClientCa
 {
     public class ShellViewModel : Screen, IShell
     {
-        private const int GwlStyle = -16;
-        private const int WsBorder = 0x00800000;
-        private const int WsChild = 0x40000000;
-        private const int SwShowmaximized = 3;
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetParent(HandleRef hWnd, HandleRef hWndParent);
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
-        
         public ObservableCollection<TabItem> TabItems { get; set; } = new ObservableCollection<TabItem>();
         private int _selectedItemIndex;
 
@@ -48,6 +31,8 @@ namespace SuperClientCa
         public int LastUsed = -1;
         public List<Process> Processes = new List<Process>();
 
+
+
         private ChildStarter _childStarter;
 
         public ShellViewModel()
@@ -61,41 +46,17 @@ namespace SuperClientCa
             _childStarter = new ChildStarter();
         }
 
-
-        private Panel CreatePanel()
+        public void AddChild()
         {
             var tabItem = new TabItem() { Header = new ContentControl() };
             TabItems.Add(tabItem);
-
-            var windowsFormsHost = new WindowsFormsHost();
-            tabItem.Content = windowsFormsHost;
-
-            Panel panel = new Panel();
+            var panel = _childStarter.CreatePanel(tabItem);
             Panels.Add(panel);
-            windowsFormsHost.Child = panel;
-
-            return panel;
-        }
-
-        private void PutChildOnPanel(IntPtr childHandle, Panel panel)
-        {
-            int oldStyle = GetWindowLong(childHandle, GwlStyle);
-            SetWindowLong(childHandle, GwlStyle, (oldStyle | WsChild) & ~WsBorder);
-            var parentHandle = new HandleRef(null, panel.Handle);
-            SetParent(new HandleRef(null, childHandle), parentHandle);
-            var childHandleRef = new HandleRef(null, childHandle);
-            ShowWindowAsync(childHandleRef, SwShowmaximized);
-        }
-
-        public void AddChild()
-        {
-            var panel = CreatePanel();
             LastUsed++;
             SelectedItemIndex = LastUsed;
             var process = _childStarter.StartChild(LastUsed, Servers[LastUsed].Item1, Servers[LastUsed].Item2);
             Processes.Add(process);
-            IntPtr childHandle = process.MainWindowHandle;
-            PutChildOnPanel(childHandle, panel);
+            _childStarter.PutChildOnPanel(process, panel);
         }
 
         public void AddServer()
